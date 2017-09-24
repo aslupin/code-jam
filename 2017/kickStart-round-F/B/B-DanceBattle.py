c5c98eb37f695988511ac2d240c1fe1a62468ace
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-09-25 00:29:30 +0700

    :tada: 100% :tada:

---
+++ 2017/kickStart-round-F/B/B-DanceBattle.py
@@ -0,0 +1,22 @@
+t = int(input())
+for ti in range(t):
+    e,n = input().split()
+    e,n = int(e),int(n)
+    h = 0
+    s = input()
+    s = s.split()
+    for i in range(len(s)):
+        s[i] = int(s[i])
+    lis = sorted(s)
+    while lis:
+        if e > lis[0]:
+            h += 1
+            e -= lis[0]
+            lis.pop(0)
+        elif len(lis)>1 and lis[len(lis)-1]+e>lis[0] and h>0:
+            e += lis[len(lis)-1] - lis[0]
+            lis.pop(len(lis)-1)
+            lis.pop(0)
+        else:
+            break
+    print(f"Case #{ti+1}:",h)
