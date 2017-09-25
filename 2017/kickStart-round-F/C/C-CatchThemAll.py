524f975701be86c56c9b2bc5d4814c9d10629e29
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-09-25 14:48:32 +0700

    ready for summit

--- 2017/kickStart-round-F/C/C-CatchThemAll.py
+++ 2017/kickStart-round-F/C/C-CatchThemAll.py
@@ -23,27 +23,29 @@ def FWShortestPath():#shortest path for all pair
             for k in range(n):
                 if tbl[i][j] > tbl[i][k] + tbl[k][j]:
                     tbl[i][j] = tbl[i][k] + tbl[k][j]
-s = input()
-s = s.split()
-n = int(s[0])
-m = int(s[1])
-p = int(s[2])
-tbl = []
-for i in range(n):
-    tbl.append([])
-    for j in range(n):
-        tbl[i].append(11)
-for i in range(n):
-    tbl[i][i] = 0
-for i in range(m):
-    s = input().split()
-    u = int(s[0])
-    v = int(s[1])
-    d = int(s[2])
-    tbl[u-1][v-1] = d
-    tbl[v-1][u-1] = d
-FWShortestPath()
-ans = 0
-find(0,p,0)
-#printtbl()
-print(ans/(n-1)**p)
+t = int(input())
+for case in range(t):
+    s = input()
+    s = s.split()
+    n = int(s[0])
+    m = int(s[1])
+    p = int(s[2])
+    tbl = []
+    for i in range(n):
+        tbl.append([])
+        for j in range(n):
+            tbl[i].append(11)
+    for i in range(n):
+        tbl[i][i] = 0
+    for i in range(m):
+        s = input().split()
+        u = int(s[0])
+        v = int(s[1])
+        d = int(s[2])
+        tbl[u-1][v-1] = d
+        tbl[v-1][u-1] = d
+    FWShortestPath()
+    ans = 0
+    find(0,p,0)
+    #printtbl()
+    print("Case #{}: {:.6f}".format(case+1,ans/(n-1)**p))
