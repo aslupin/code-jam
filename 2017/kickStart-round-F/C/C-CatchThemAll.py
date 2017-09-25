e4753d04d17842447730359b6d35f1c53f831f4b
Wattanai Sathuphan <nine.stay27@gmail.com>
2017-09-25 14:15:15 +0700

    Almost done :)

--- 2017/kickStart-round-F/C/C-CatchThemAll.py
+++ 2017/kickStart-round-F/C/C-CatchThemAll.py
@@ -1,15 +1,28 @@
 def find(f,p,s):
     p -= 1
-    global ans,c
     for i in range(n):
         if i==f or tbl[f][i]==11:
             continue
         if(p>0):
             find(i,p,s+tbl[f][i])
         else:
-            c += 1
+            global ans
             ans += tbl[f][i] + s
-            print(f"{f} -> {i} = {tbl[f][i]}",ans)
+            #print(f"{f} -> {i} = {tbl[f][i]}",ans)
+
+def printtbl():#print tbl
+    for i in range(n):
+        for j in range(n):
+            print(tbl[i][j],end=' ')
+        print()
+
+def FWShortestPath():#shortest path for all pair
+    global tbl
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if tbl[i][j] > tbl[i][k] + tbl[k][j]:
+                    tbl[i][j] = tbl[i][k] + tbl[k][j]
 s = input()
 s = s.split()
 n = int(s[0])
@@ -29,18 +42,8 @@ for i in range(m):
     d = int(s[2])
     tbl[u-1][v-1] = d
     tbl[v-1][u-1] = d
-#shortest path
-for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            if tbl[i][j] > tbl[i][k] + tbl[k][j]:
-                tbl[i][j] = tbl[i][k] + tbl[k][j]
+FWShortestPath()
 ans = 0
-c = 0
 find(0,p,0)
-#test print
-for i in range(n):
-    for j in range(n):
-        print(tbl[i][j],end=' ')
-    print()
-print(c,ans)
+#printtbl()
+print(ans/(n-1)**p)
